008452626c3f670153259590edf8f87244ca572d
Jorge Sanz <xurxosanz@gmail.com>
2019-01-23 19:41:36 +0100

    added airship and dashboard

---
+++ assets/dashboard.js
@@ -0,0 +1,43 @@
+// hello
+
+function _toggleDrawer() {
+    document
+        .querySelector('.as-toolbar__actions')
+        .classList.toggle('as-toolbar__actions--visible');
+}
+
+
+function _toggleAbout() {
+    document
+        .querySelector('.js-about')
+        .classList.toggle('js-about--hidden');
+}
+
+function refreshData(variables) {
+    vm.companiesCount = variables.companiesCount.value
+    vm.companiesData = variables.companiesData.value.map(function (entry) {
+        if (entry && entry.properties) {
+            const props = entry.properties;
+            return {
+                'cartodb_id': props.cartodb_id,
+                'name': props.name,
+                'category': props.category,
+                'location': props.location
+            }
+        } else {
+            return null
+        }
+    })
+}
+
+function loadDashboard(vm, data) {
+    const viz = data.noClusterLayer.viz;
+    const layer = data.noClusterLayer.layer;
+
+    layer.on('updated', function () {
+        refreshData(viz.variables);
+    })
+    layer.on('loaded', function () {
+        refreshData(viz.variables);
+    })
+}
--- assets/map.js
+++ assets/map.js
@@ -1,7 +1,3 @@
-function hideLoader() {
-    document.getElementById('loader').style.opacity = '0';
-  }
-
 function loadMap() {
     const map = new mapboxgl.Map({
         container: 'map',
@@ -9,23 +5,26 @@ function loadMap() {
         center: [2.5, 33],
         zoom: 1.3,
         scrollZoom: false,
-        hash: true
-      });
-  
-      const nav = new mapboxgl.NavigationControl({
+        hash: true,
+        customAttribution: [
+            '<a target="_blank" href="http://geoawesomeness.com">Geoawesomeness</a>'
+        ]
+    });
+
+    const nav = new mapboxgl.NavigationControl({
         showCompass: false
-      });
-      map.addControl(nav, 'top-left');
-    
-      // Define user
-      carto.setDefaultAuth({
+    });
+    map.addControl(nav, 'top-left');
+
+    // Define user
+    carto.setDefaultAuth({
         username: 'stephaniemongon',
         apiKey: 'VMWFFLIx15oGjDIBfzVTgw'
-      });
-  
-      // Define layer
-      const source = new carto.source.Dataset('top_companies_full');
-      const viz = new carto.Viz(`
+    });
+
+    // Define layer
+    const source = new carto.source.Dataset('top_companies_full');
+    const viz = new carto.Viz(`
         @size: sqrt(clusterCount()) * 3.5
         @size2: sqrt(clusterCount()) * 7
   
@@ -36,9 +35,29 @@ function loadMap() {
         strokeWidth: .5
         resolution: 8
       `);
-      const layer = new carto.Layer('layer', source, viz);
-  
-      layer.addTo(map, 'watername_ocean');
-      layer.on('loaded', hideLoader);
+    const layer = new carto.Layer('layer', source, viz);
+    layer.addTo(map, 'watername_ocean');
+
+    const noClusterViz = new carto.Viz(`
+        @companiesCount: viewportCount()
+        @companiesData: viewportFeatures($cartodb_id, $name, $category, $location)
+
+        color: opacity(red, 0.00001)
+        strokeWidth: 0
+      `);
+    const noClusterLayer = new carto.Layer('unclusteredLayer', source, noClusterViz);
+
+    noClusterLayer.addTo(map, 'layer');
+
+    return {
+        'map': map,
+        'clusterLayer': {
+            'viz': viz,
+            'layer': layer
+        },
+        'noClusterLayer': {
+            'viz': noClusterViz,
+            'layer': noClusterLayer
+        }
+    }
 }
- 
