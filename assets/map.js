075c4ee4f69973e75ddcf2231e31e78a37333541
ztephm <ztephmtwitter@gmail.com>
2019-01-24 10:52:39 -0500

    getting rid of data-driven legend to style labels & markers differently

--- assets/map.js
+++ assets/map.js
@@ -29,12 +29,13 @@ function loadMap() {
         @c_count: clusterCount()
         @size: sqrt(@c_count) * 3
         @size2: @c_count * 6
-        @colorRamp: ramp(buckets(@c_count, [1, 2, 3, 4, 5, 6, 7, 8]), sunset)
+        @colorRamp: ramp(buckets(@c_count, [1, 2, 3, 4, 5, 6, 7, 8]), sunset) // MANUAL CLASSIFICATION
         @colorOpacity: 0.7
+        @strokeOpacity: 0.7
 
         width: ramp(zoomrange([2,4]),[@size,@size2])
         color: opacity(@colorRamp, @colorOpacity)
-        strokeColor: opacity(ramp(buckets(@c_count, [1, 2, 3, 4, 5, 6, 7, 8]), sunset) 0.9)
+        strokeColor: opacity(@colorRamp @strokeOpacity)
         strokeWidth: .5
         resolution: 8
       `);
@@ -45,25 +46,23 @@ function loadMap() {
       layer.on('loaded', () => {
         hideLoader();
 
-        //** ADD LEGEND **//
-        // Request data for legend from the layer viz variables 'myRamp' and 'myOpacity'
-        const colorLegend = layer.viz.variables.colorRamp.getLegendData();
-        const opacity = layer.viz.variables.colorOpacity.value;
+        //** MANUAL CLASSIFICATION LEGEND **//
+      //   const colorLegend = layer.viz.variables.colorRamp.getLegendData();
+      //   const opacity = layer.viz.variables.colorOpacity.value;
 
-        let colorLegendList = '';
+      //   let colorLegendList = '';
 
-        // Create list elements for legend
-        colorLegend.data.forEach((legend, index) => {
-          const color = legend.value;
-          // Add the predefined opacity to the ramp color components
-          const rgba = `rgba(${color.r}, ${color.g}, ${color.b}, ${opacity})`;
-          // Style for legend items based on geometry type
-          colorLegendList +=
-            `<li><span class="point-mark" style="background-color:${rgba}; border: 1px solid black;"></span><span>${legend.key}</span></li>\n`;
-          // Place list items in the content section of the title/legend box
-          document.getElementById('content').innerHTML = colorLegendList;
-        });
+      //   colorLegend.data.forEach((legend, index) => {
+      //     const color = legend.value;
+      //     const rgba = `rgba(${color.r}, ${color.g}, ${color.b}, ${opacity})`;
 
+      //     console.log(color);
+      //     console.log(rgba);
+
+      //     colorLegendList +=
+      //       `<li><span class="point-mark" style="background-color:${rgba}; border: 1px solid black;"></span><span>${legend.key}</span></li>\n`;
+      //     document.getElementById('content').innerHTML = colorLegendList;
+      //   });
       });
       
 }
